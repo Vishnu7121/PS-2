ebb840879de27ee000b4f682cb413cd1c3efa088
Vishnu7121 <127678489+Vishnu7121@users.noreply.github.com>
2023-07-04 22:29:46 +0530

    Add files via upload

---
+++ def.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Log file for capturing all output and errors in the home directory
+LOG_FILE=~/def.log
+
+# Function to log a message with a timestamp
+log() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" >> "$LOG_FILE"
+}
+
+# Redirect standard output and standard error to the log file
+exec > >(tee -a "$LOG_FILE")
+exec 2> >(tee -a "$LOG_FILE" >&2)
+
+log "Starting script..."
+
+log "Changing directory to the home directory"
+cd
+
+log "Copying LoginService.java from S3"
+aws s3 cp s3://sql/LoginService.java ~
+
+log "Changing directory to the LoginService path"
+cd srikanth/SQL-Injection/src/com/app/login
+
+log "Removing existing LoginService.java"
+sudo rm LoginService.java
+
+log "Changing directory back to the home directory"
+cd
+sudo mv ~/LoginService.java srikanth/SQL-Injection/src/com/app/login/
+
+cd
+
+log "Changing directory to srikanth/SQL-Injection/"
+cd srikanth/SQL-Injection/
+
+log "Running 'sudo ant -d clean compile dist'"
+sudo ant -d clean compile dist
+
+result=$(sudo docker images -q sql-injection_web)
+if [[ -n "$result" ]]; then
+    log "Docker image exists. Stopping and removing existing containers."
+    sudo docker-compose down --volume --rmi all
+else
+    log "No such Docker image found."
+fi
+
+log "Building the Docker image"
+sudo docker-compose up -d
+
+log "Script execution completed."
---
+++ lab.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Install Docker
+#echo "Installing Docker..."
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+#sudo usermod -aG docker "$USER"
+#sudo systemctl enable docker
+#sudo systemctl start docker
+
+# Install Docker Compose (version 1.26.2)
+#echo "Installing Docker Compose..."
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+
+## Install Ant 1.10.12
+#echo "Installing Ant 1.10.12..."
+#sudo apt-get update
+#sudo apt-get install -y ant
+
+# Install Git
+#echo "Installing Git..."
+#sudo apt-get install -y git
+DIR=./srikanth
+
+if [ -e "$DIR" ]; then
+   sudo rm -rf "$DIR"
+fi
+
+if [ -d "$DIR" ]; then
+    echo "Directory exists"
+else
+    sudo mkdir "$DIR"
+fi
+
+cd "$DIR" || exit
+
+echo "Cloning SQL-Injection"
+sudo git clone https://github.com/srikanthreddy-lectures/SQL-Injection.git
+
+cd ./SQL-Injection/ || exit
+
+sudo ant -d clean compile dist
+
+result=$(sudo docker images -q sql-injection_web)
+
+if [[ -n "$result" ]]; then
+    echo "Image exists"
+    sudo docker-compose down --volume --rmi all
+else
+    echo "No such image"
+fi
+
+# Build the Docker image
+#echo "Building the Docker image"
+#sudo docker-compose build
+
+# Create the test.war file
+#echo "Creating test.war file..."
+#sudo touch ./dist/test.war
+
+# Start the Docker containers
+echo "Starting the Docker containers"
+sudo docker-compose up -d
